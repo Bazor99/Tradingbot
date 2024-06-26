689a9c3cc078f824cabbe2211bb665cfeb654796
bazor99 <k1ngx13y@gmail.com>
2024-06-26 19:17:16 +0100

    first update

---
+++ template.py
@@ -0,0 +1,34 @@
+import os
+from pathlib import Path
+import logging
+
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+
+list_of_files = [
+    "tradingbot/__init__.py",
+    "tradingbot/helper.py",
+    "tradingbot/prompt.py",
+    ".env",
+    "requirements.txt",
+    "setup.py",
+    "research/test.ipynb",
+    "app.py",
+]
+
+
+for filpath in list_of_files:
+    filepath = Path(filpath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} already exists")
